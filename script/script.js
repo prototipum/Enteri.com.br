0057c2b8a0fb60d8c33e261a4471e2578d1e1e34
Ygor Scofano <cinzeiro@outlook.com.br>
2025-12-18 13:42:53 -0300

    Ethapa

--- script/script.js
+++ script/script.js
@@ -9,3 +9,49 @@ function stickyHeader(header) {
     });
 };
 stickyHeader(header);
+function clean() {
+    const elementsHtml = document.querySelectorAll('*');
+    elementsHtml.forEach(element => {
+        element.addEventListener('click', function(event) {
+            event.preventDefault();
+            event.target.remove();
+        });
+    });
+    console.log('clique em algum lugar da página!')
+};
+console.log('digite clean()');
+function slideShow() {
+    const buttons = document.querySelectorAll('.button_slide');
+    const imagesSlide = document.querySelectorAll('.slide_image');
+    imagesSlide[0].classList.add('active_slide');
+    buttons.forEach((button, index) => {
+		button.addEventListener('click', function () {
+			imagesSlide.forEach((img) => img.classList.remove("active_slide"));
+			imagesSlide[index].classList.add("active_slide");
+		});
+	});
+};
+function arrowFunction() {
+    const arrowLeft = document.querySelector('.arrow_left');
+    const arrowRight = document.querySelector('.arrow_right');
+    const imagesSlide = document.querySelectorAll('.slide_image');
+    let currentIndex = 0;
+    arrowLeft.addEventListener('click', function () {
+        imagesSlide[currentIndex].classList.remove("active_slide");
+        currentIndex--;
+        if (currentIndex < 0) {
+            currentIndex = imagesSlide.length - 1;
+        }
+        imagesSlide[currentIndex].classList.add("active_slide");
+    });
+    arrowRight.addEventListener('click', function () {
+        imagesSlide[currentIndex].classList.remove("active_slide");
+        currentIndex++;
+        if (currentIndex >= imagesSlide.length) {
+            currentIndex = 0;
+        }
+        imagesSlide[currentIndex].classList.add("active_slide");
+    });
+};
+arrowFunction();
+slideShow();
